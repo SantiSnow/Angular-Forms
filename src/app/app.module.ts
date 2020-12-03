ea89a55d7d9ccb195810ca62ab0c75a9afcb5adb
Santiago Aguirre <aguirresantiago81@gmail.com>
2020-12-03 14:43:30 -0300

    Agregados algunos componentes y rutas a la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Route[] = [
+  { path: '', component: HomeComponent},
+  { path: '/about-us', component: AboutUsComponent},
+  { path: '/contact', component: ContactComponent},
+  { path: '/register', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
