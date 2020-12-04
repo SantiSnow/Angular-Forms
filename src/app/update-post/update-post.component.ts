d54526ee5d8c4649416065e65673868938efe629
Santiago Aguirre <aguirresantiago81@gmail.com>
2020-12-04 12:58:36 -0300

    Agregado al componente de actualizacion el metodo update del servicio

--- src/app/update-post/update-post.component.ts
+++ src/app/update-post/update-post.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from './../post';
+import { PostsService } from './../posts.service';
+
 @Component({
   selector: 'app-update-post',
   templateUrl: './update-post.component.html',
   styleUrls: ['./update-post.component.css']
 })
-export class UpdatePostComponent implements OnInit {
+export class UpdatePostComponent {
 
-  constructor() { }
+  constructor(private postService: PostsService) { }
 
-  ngOnInit(): void {
+
+  updatePost(post: Post){
+    this.postService.updatePost(post).subscribe(
+      res=>{
+        console.log(res);
+      },
+      err=>{
+        console.log(err);
+      }
+    );
   }
 
 }
